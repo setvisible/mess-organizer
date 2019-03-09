d3d5da303125973f4721a66515045fe5e996c04f
setvisible <sebastien.vavassori@gmail.com>
2019-03-10 00:01:29 +0100

    Add class SoftwareInfo (wip)

---
+++ src/main/java/com/github/setvisible/messorganizer/core/SoftwareInfo.java
@@ -0,0 +1,13 @@
+package com.github.setvisible.messorganizer.core;
+
+/**
+ * The class {@link SoftwareInfo} contains info about a given software setup
+ * file.
+ * <p>
+ * A <i>software</i> is an setup file (executable) or an archive containing a
+ * setup.
+ *
+ */
+public class SoftwareInfo {
+
+}
